4dcd1145d38a4500504b3db255b00c358c7fdd64
srimoy26 <srimoy.ranjandas@india.benesse.com>
2023-09-26 13:19:59 +0530

    Created a custom command to add student in the students table

---
+++ app/Console/Commands/AddStudent.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Student;
+
+class AddStudent extends Command
+{
+    protected $signature = 'student:add {first_name} {last_name} {email} {phone}';
+    protected $description = 'Add a new student to the student table';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        $firstName = $this->argument('first_name');
+        $lastName = $this->argument('last_name');
+        $email = $this->argument('email');
+        $phone = $this->argument('phone');
+
+        
+        $student = new Student([
+            'first_name' => $firstName,
+            'last_name' => $lastName,
+            'email' => $email,
+            'phone' => $phone,
+        ]);
+
+        $student->save();
+
+        $this->info('Student added successfully.');
+    }
+}
